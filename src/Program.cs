4b56f83900f66ecc94d30000d7641c060c394e27
yahyoHakimov <yahyohakimov7@gmail.com>
2025-08-28 11:48:11 +0500

    second task is done

--- src/Program.cs
+++ src/Program.cs
@@ -1,25 +1,24 @@
-// First Task
-//Biz bu taskda Redis bilan aloqa qilishimiz uchun kerakli bo'lgan TCP yo'lni ochish va yo'l raqamini tanishitirishimiz kerak. 
-//Yani siz agar Redis ishlatgan bo'lsangiz siz terminaldan foydalangansiz. 
-//Shu termminalda beriladigan commandlar esa sizni redis bilan bog'lagan. 
-//Bu commandlar esa Tcp esa reliable protocol yani sizni commandlarizi aytilgandek ketishiga yordam beradi.
-// Keyin esa bizni commandlar qayesi yo'l orqali ketishini aytish uchun port ham biriktiramiz
+//Task 2
 
-//Bu kabi texnik so'zlarni tushunmaydigan do'stlarimiz uchun esa biz hayotiy misollar  ham keltiramiz. Tassavur qiling siz Redis nomli restoran ochmoqchisiz. 
-//Siz bu restoranni daromadi yo'q va mijozi yo'q holatdan daromadli va mijozi ko'p bo'ladigan restoranga aylantirmoqchisiz. 
-//Bizni bu 1 - Task bu Redis restoran eshigiga raqam qo'yish. Yani port 6732 raqamli uy bizni Redis resotranimiz eshik raqami bo'ladi. 
-//Task dagi tester esa bizni tekshirib ko'radi yani Tester kelganda eshikni taqqilatadi va eshik ochilishi yani javob kelishi kerak. 
+//Bu taskda biz Redis ishlayotganini bilishimiz uchun kerak bo'ladigan PING commandni ishlatib ko'rishimiz kerak, va javob tarzida PONG jo'natsak bo'ladi
 
+//Hayotiy misolda esa
+
+//Redis restaurant esa ishni boshladi, va biz eshik oldida hamma mijozlarga "xush kelibsiz, marahamat" deyish uchun ishchi yolladik. 
+//Bu taskda esa mijoz eshik yoniga keladi va so'raydi "Sizlar ishlayapsizlarmi" xodim esa so'rov nimaligiga etibor bermasdan "Biz ishlayapmiz
+// PONG nomli javob beradi"
 
 //Boshladik
 
-Console.WriteLine("Hey, biz Redis restoranini ochdik va mijozlarga xizmat ko'rsatishni boshlaganmiz. Eshik raqamimiz esa 6739: ");
-
-//Biz bu yerda Redis restoranini mijozlar topib kelishi uchun raqamladik.
+Console.WriteLine("Sizni mijoz bilan aloqangiz shu yerda ko'rinadi.");
 TcpListener server = new TcpListener(IPAddress.Any, 6739);
-
-//Restoranni esa sahar ochdik
 server.Start();
 
-// Va mijozlar qabul qilishni boshladi
-server.AcceptSocket();
+var client = server.AcceptSocket();
+
+string message = "+PONG\r\n";
+
+//bu code bizga inson yozilgan yozuvlarni protokol tushunadigan byte larga o'girib beradi. 
+byte[] data = Encoding.UTF8.GetBytes(message);
+
+client.Send(data);
